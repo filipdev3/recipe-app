47a5cc18e3981a2bd17f7775d83b6fc4857c819f
Filip Kulic <filip.kulic.dev@outlook.com>
2025-05-13 09:25:11 +0200

    Added loader while fetching recipe and scroll into view when its ready

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,12 +2,15 @@ import { useEffect, useRef, useState } from "react"
 import Recipe from "./Recipe"
 import IngredientsList from "./IngredientsList"
 import { getRecipeFromMistral } from '../ai'
+import { MoonLoader } from "react-spinners"
 
 export default function MainContent() {
 
     const [recipe, setRecipe] = useState()
     const [ingredients, setIngredients] = useState([])
     const recipeSection = useRef(null)
+
+    const [loading, setLoading] = useState()
     
     useEffect(() => {
         if(recipe !== "" && recipeSection.current !== null){
@@ -26,8 +29,15 @@ export default function MainContent() {
 
     {/*function for getting a recipe*/}
     async function getRecipe(){
-        const recipeMarkdown = await getRecipeFromMistral(ingredients)
-        setRecipe(recipeMarkdown)
+        setLoading(true)
+        try{
+            const recipeMarkdown = await getRecipeFromMistral(ingredients)
+            setRecipe(recipeMarkdown)
+        } catch(err){
+            console.log("Failed to fetch recipe: ", err)
+        } finally {
+            setLoading(false)
+        }
     }
 
 
@@ -60,7 +70,10 @@ export default function MainContent() {
 
 
             {/*display Recipe component if recipe is truthy value (not undefined/null/empty string)*/}
-            { recipe ? <Recipe recipe={recipe}/> : null }
+
+            { loading && <div className="loader-container"><MoonLoader /></div> }
+
+            { !loading && recipe ? <Recipe recipe={recipe}/> : null }
 
         </main>
     )
